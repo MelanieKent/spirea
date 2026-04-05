605a106bd1b53f32c6b54a2a4086d6ed4e4fbabc
Melanie Kent <mskent@live.ca>
2026-04-05 16:03:26 -0700

    make strategic applications page mobile responsive

--- src/pages/StrategicApplications.jsx
+++ src/pages/StrategicApplications.jsx
@@ -1,7 +1,7 @@
 export const StrategicApplications = () => {
   return (
     <div
-      className="h-screen w-screen pt-0 flex items-center justify-center text-xl"
+      className="min-h-screen w-screen pt-0 flex flex-col items-center justify-center text-xl p-4"
       style={{
         backgroundImage: `url('${process.env.PUBLIC_URL}/strategic-applications-background.png')`,
         backgroundSize: "cover",
@@ -10,7 +10,7 @@ export const StrategicApplications = () => {
     >
       <div className="flex flex-col items-center justify-center gap-12 max-w-[80rem]">
         <div className="flex flex-col gap-4">
-          <h1 className="text-4xl">Strategic Applications</h1>
+          <h1 className="text-4xl mt-32 md:mt-0">Strategic Applications</h1>
           <h4>Scalable infrastructure systems designed for resilience, adaptability, and rapid deployment across diverse environments.</h4>
         </div>
         <div className="flex flex-col gap-4">
@@ -19,20 +19,20 @@ export const StrategicApplications = () => {
             <h4 className="text-2xl text-nowrap">3 Core Pillars</h4>
             <hr className="border-t border-[#829276] opacity-40 w-full"></hr>
           </div>
-          <div className="grid grid-cols-3 gap-6">
-            <div className="flex flex-col justify-center items-center gap-2 w-80 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
+          <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+            <div className="flex flex-col justify-center items-center gap-2 w-72 md:w-80 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
               <img className="w-10" src={`${process.env.PUBLIC_URL}/resilient-icon.png`}/>
               <h6>Resilient Infrastructure Systems</h6>
               <hr className="border-t border-[#829276] opacity-40 w-full"></hr>
               <p className="text-base">Designed for performance in demanding climates and conditions.</p>
             </div>
-            <div className="flex flex-col justify-center items-center gap-2 w-80 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
+            <div className="flex flex-col justify-center items-center gap-2 w-72 md:w-80 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
               <img className="w-10" src={`${process.env.PUBLIC_URL}/rapid-icon.png`}/>
               <h6>Rapid Deployment Frameworks</h6>
               <hr className="border-t border-[#829276] opacity-40 w-full"></hr>
               <p className="text-base">Enabling faster construction cycles through integrated production and modular assembly.</p>             
             </div>
-            <div className="flex flex-col justify-center items-center gap-2 w-80 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
+            <div className="flex flex-col justify-center items-center gap-2 w-72 md:w-80 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
               <img className="w-10" src={`${process.env.PUBLIC_URL}/remote-icon.png`}/>
               <h6>Remote & Distributed Development</h6>
               <hr className="border-t border-[#829276] opacity-40 w-full"></hr>

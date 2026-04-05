deb31076fe455e9194766cad641467bf91e72096
Melanie Kent <mskent@live.ca>
2026-04-05 16:24:36 -0700

    make about page responsive pt.2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ export const About = () => {
   return (
     <>
       <div
-        className="h-[90vh] w-screen pt-0 flex flex-col items-center justify-center text-xl"
+        className="min-h-[90vh] w-screen pt-0 flex flex-col items-center justify-center text-xl p-4"
         style={{
           backgroundImage: `url('${process.env.PUBLIC_URL}/about-background-p1.png')`,
           backgroundSize: "cover",
@@ -20,21 +20,21 @@ export const About = () => {
           </h1>
           <p>Spirea Platform is a construction technology platform integrating modular construction logic, additive manufacturing, and bio-based material systems.</p>
           <p>The platform is designed to enable scalable, resilient, and low-carbon infrastructure across diverse environments - from housing to remote and strategic applications.</p>
-          <div className="mt-8 flex gap-4 h-12">
+          <div className="mt-8 flex gap-4 h-10 sm:h-12 text-sm sm:text-xl">
             <PrimaryButton>Explore Prototype V1  →</PrimaryButton>
             <SecondaryButton>Strategic Application  →</SecondaryButton>
           </div>
         </div>
       </div>
       <div
-        className="h-[50vh] w-screen pt-0 text-xl flex flex-col items-center justify-center gap-32"
+        className="min-h-[50vh] w-screen pt-0 text-xl flex flex-col items-center justify-center gap-32 p-4"
         style={{
           backgroundImage: `url('${process.env.PUBLIC_URL}/about-background-p2.png')`,
           backgroundSize: "cover",
           backgroundPosition: "center"
         }}
       >
-        <div className="flex flex-col gap-6 max-w-[60rem]">
+        <div className="flex flex-col gap-6 max-w-[60rem] mt-16 sm:mt-0">
           <h1 className="text-4xl">Rethinking Construction as a System</h1>
           <p>The built environment is under increasing pressure to deliver at scale while reducing carbon and improving resilience.</p>
           <p>Spirea addresses this challenge by treating construction as an integrated system - combining material innovation, controlled production, and modular deployment into a unified approach.</p>

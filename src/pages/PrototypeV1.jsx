371b203adec88f7cb076d2dac84d14ef1ab5d172
Melanie Kent <mskent@live.ca>
2026-04-05 16:25:00 -0700

    make prototype v1 page responsive pt.2

--- src/pages/PrototypeV1.jsx
+++ src/pages/PrototypeV1.jsx
@@ -2,14 +2,14 @@ export const PrototypeV1 = () => {
   return (
     <>
       <div
-        className="h-screen w-screen pt-0 flex flex-col items-center justify-center text-xl text-left"
+        className="min-h-screen w-screen pt-0 flex flex-col items-center justify-center text-xl text-left p-4"
         style={{
           backgroundImage: `url('${process.env.PUBLIC_URL}/prototype-v1-background-p1.png')`,
           backgroundSize: "cover",
           backgroundPosition: "center"
         }}
       >
-        <div className="flex flex-col items-start justify-center gap-48">
+        <div className="flex flex-col items-start justify-center md:gap-44 mt-32 md:mt-0">
           <div className="flex flex-col gap-6">
             <h4>PROTOTYPE V1</h4>
             <h1 className="text-4xl">Integrated Wall System <br />for Modular, Low-Carbon Construction</h1>
@@ -19,18 +19,20 @@ export const PrototypeV1 = () => {
             </div>
           </div>
           <div className="flex flex-col gap-8">
-            <div className="grid grid-cols-3 gap-6">
-              <div className="flex flex-col justify-center items-center gap-2 w-72 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
-                <img className="w-12" src={`${process.env.PUBLIC_URL}/additive-icon.png`}/>
-                <h6>Additive Structure</h6>
-              </div>
-              <div className="flex flex-col justify-center items-center gap-2 w-72 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
-                <img className="w-12" src={`${process.env.PUBLIC_URL}/bio-icon.png`}/>
-                <h6>Bio-Based Insulation</h6>
-              </div>
-              <div className="flex flex-col justify-center items-center gap-2 w-72 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
-                <img className="w-12" src={`${process.env.PUBLIC_URL}/modular-icon.png`}/>
-                <h6>Modular Assembly</h6>
+            <div className="flex flex-col items-center">
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-8">
+                <div className="flex flex-col justify-center items-center gap-2 w-72 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
+                  <img className="w-12" src={`${process.env.PUBLIC_URL}/additive-icon.png`}/>
+                  <h6>Additive Structure</h6>
+                </div>
+                <div className="flex flex-col justify-center items-center gap-2 w-72 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
+                  <img className="w-12" src={`${process.env.PUBLIC_URL}/bio-icon.png`}/>
+                  <h6>Bio-Based Insulation</h6>
+                </div>
+                <div className="flex flex-col justify-center items-center gap-2 w-72 bg-[#FAF5F3] rounded-lg hover:bg-[#F1EBE6] p-8 shadow-lg">
+                  <img className="w-12" src={`${process.env.PUBLIC_URL}/modular-icon.png`}/>
+                  <h6>Modular Assembly</h6>
+                </div>
               </div>
             </div>
             <p className="min-width">Prototype V1 marks a step toward a scalable construction platform — <br />integrating material innovation, controlled production, and modular deployment.</p>

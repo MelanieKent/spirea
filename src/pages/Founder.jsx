dbf6c7158f75cdff0b9bfb966b4b5c62f47354ff
Melanie Kent <mskent@live.ca>
2026-04-05 15:52:00 -0700

    make founder page mobile responsive

--- src/pages/Founder.jsx
+++ src/pages/Founder.jsx
@@ -1,17 +1,17 @@
 export const Founder = () => {
   return (
     <div
-      className="h-screen w-screen pt-0 flex flex-col items-center justify-center text-xl gap-48"
+      className="min-h-screen w-screen pt-0 flex flex-col items-center justify-center text-xl gap-48 p-4"
       style={{
         backgroundImage: `url('${process.env.PUBLIC_URL}/founder-background.png')`,
         backgroundSize: "cover",
         backgroundPosition: "center"
       }}
     >
-      <div className="grid grid-cols-[2fr_1fr] max-w-[80rem] text-left">
+      <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] max-w-[80rem] text-left">
         <div className="flex flex-col gap-6">
           <div>
-            <h1 className="text-4xl">
+            <h1 className="text-4xl mt-32 md:mt-0">
               Julia Liu
             </h1>
             <p>Founder</p>
@@ -22,7 +22,7 @@ export const Founder = () => {
           <p>Spirea Platform reflects a long-term vision to transform construction into a systemized, scalable, and climate-aligned industry - enabling deployment across diverse environments and applications.</p>
         </div>
         <div className="flex items-center justify-center">
-          <img className="w-64" src={`${process.env.PUBLIC_URL}/headshot.png`} />
+          <img className="w-64 py-8" src={`${process.env.PUBLIC_URL}/headshot.png`} />
         </div>
       </div>
       <p className="italic">A platform for the next generation of infrastructure.</p>

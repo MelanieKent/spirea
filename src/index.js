b41397e39186821e910c689cad95b60c850d8070
Melanie Kent <mskent@live.ca>
2026-04-05 18:46:26 -0700

    implement ScrollToTop component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from './components/ScrollToTop';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
